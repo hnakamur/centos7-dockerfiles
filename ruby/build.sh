ddff9ead7f0d40b9bcf8978e710c73a28e4faeb6
Hiroaki Nakamura <hnakamur@gmail.com>
2014-11-09 17:50:57 +0900

    Simplify ruby Dockerfile with adding pre-built ruby rpm

--- ruby/build.sh
+++ ruby/build.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-if [ ! $1 ]; then
-  echo "Usage: $0 private_repo_ip" 1>&2
-  exit 1
-fi
-dir=`dirname $0`
-awk -v url=http://$1/private-centos7-repo/x86_64/ '{ gsub("{{ private_repo_url }}", url); print $0 }' $dir/private.repo.base > $dir/private.repo
-docker build -t hnakamur/ruby $dir
+docker build -t hnakamur/ruby `dirname $0`
